a3e3f42d894ad81a034511d92a42d181033baf38
Jewfro193 <eliwss0@gmail.com>
2020-08-22 15:03:36 -0500

    Add project files.

---
+++ PhotoOrganizer.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace photo_organizer {
+    partial class PhotoOrganizer {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing&&(components!=null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel8 = new System.Windows.Forms.FlowLayoutPanel();
+            this.directoryButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel7 = new System.Windows.Forms.FlowLayoutPanel();
+            this.catagory2Button = new System.Windows.Forms.Button();
+            this.flowLayoutPanel6 = new System.Windows.Forms.FlowLayoutPanel();
+            this.catagory3Button = new System.Windows.Forms.Button();
+            this.flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.catagory3TextBox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.catagory2TextBox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.catagory1TextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.catagory1Button = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.directoryTextBox = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Filename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ParentFolder = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel8.SuspendLayout();
+            this.flowLayoutPanel7.SuspendLayout();
+            this.flowLayoutPanel6.SuspendLayout();
+            this.flowLayoutPanel5.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel8, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel7, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel6, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1145, 645);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel8
+            // 
+            this.flowLayoutPanel8.Controls.Add(this.directoryButton);
+            this.flowLayoutPanel8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel8.Location = new System.Drawing.Point(425, 615);
+            this.flowLayoutPanel8.Name = "flowLayoutPanel8";
+            this.flowLayoutPanel8.Size = new System.Drawing.Size(416, 27);
+            this.flowLayoutPanel8.TabIndex = 12;
+            // 
+            // directoryButton
+            // 
+            this.directoryButton.AutoSize = true;
+            this.directoryButton.Location = new System.Drawing.Point(3, 3);
+            this.directoryButton.Name = "directoryButton";
+            this.directoryButton.Size = new System.Drawing.Size(100, 23);
+            this.directoryButton.TabIndex = 0;
+            this.directoryButton.Text = "Choose folder";
+            this.directoryButton.UseVisualStyleBackColor = true;
+            this.directoryButton.Click += new System.EventHandler(this.directoryButton_Click);
+            // 
+            // flowLayoutPanel7
+            // 
+            this.flowLayoutPanel7.Controls.Add(this.catagory2Button);
+            this.flowLayoutPanel7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel7.Location = new System.Drawing.Point(425, 551);
+            this.flowLayoutPanel7.Name = "flowLayoutPanel7";
+            this.flowLayoutPanel7.Size = new System.Drawing.Size(416, 26);
+            this.flowLayoutPanel7.TabIndex = 11;
+            // 
+            // catagory2Button
+            // 
+            this.catagory2Button.AutoSize = true;
+            this.catagory2Button.Location = new System.Drawing.Point(3, 3);
+            this.catagory2Button.Name = "catagory2Button";
+            this.catagory2Button.Size = new System.Drawing.Size(75, 23);
+            this.catagory2Button.TabIndex = 2;
+            this.catagory2Button.Text = "Catagory 2";
+            this.catagory2Button.UseVisualStyleBackColor = true;
+            this.catagory2Button.Click += new System.EventHandler(this.catagory2Button_Click);
+            // 
+            // flowLayoutPanel6
+            // 
+            this.flowLayoutPanel6.Controls.Add(this.catagory3Button);
+            this.flowLayoutPanel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel6.Location = new System.Drawing.Point(425, 583);
+            this.flowLayoutPanel6.Name = "flowLayoutPanel6";
+            this.flowLayoutPanel6.Size = new System.Drawing.Size(416, 26);
+            this.flowLayoutPanel6.TabIndex = 10;
+            // 
+            // catagory3Button
+            // 
+            this.catagory3Button.AutoSize = true;
+            this.catagory3Button.Location = new System.Drawing.Point(3, 3);
+            this.catagory3Button.Name = "catagory3Button";
+            this.catagory3Button.Size = new System.Drawing.Size(75, 23);
+            this.catagory3Button.TabIndex = 3;
+            this.catagory3Button.Text = "Catagory 3";
+            this.catagory3Button.UseVisualStyleBackColor = true;
+            this.catagory3Button.Click += new System.EventHandler(this.catagory3Button_Click);
+            // 
+            // flowLayoutPanel5
+            // 
+            this.flowLayoutPanel5.Controls.Add(this.catagory3TextBox);
+            this.flowLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel5.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel5.Location = new System.Drawing.Point(3, 583);
+            this.flowLayoutPanel5.Name = "flowLayoutPanel5";
+            this.flowLayoutPanel5.Size = new System.Drawing.Size(416, 26);
+            this.flowLayoutPanel5.TabIndex = 9;
+            // 
+            // catagory3TextBox
+            // 
+            this.catagory3TextBox.Location = new System.Drawing.Point(163, 3);
+            this.catagory3TextBox.Name = "catagory3TextBox";
+            this.catagory3TextBox.Size = new System.Drawing.Size(250, 20);
+            this.catagory3TextBox.TabIndex = 2;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.catagory2TextBox);
+            this.flowLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel4.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(3, 551);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(416, 26);
+            this.flowLayoutPanel4.TabIndex = 8;
+            // 
+            // catagory2TextBox
+            // 
+            this.catagory2TextBox.Location = new System.Drawing.Point(163, 3);
+            this.catagory2TextBox.Name = "catagory2TextBox";
+            this.catagory2TextBox.Size = new System.Drawing.Size(250, 20);
+            this.catagory2TextBox.TabIndex = 1;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.catagory1TextBox);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel3.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 519);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(416, 26);
+            this.flowLayoutPanel3.TabIndex = 7;
+            // 
+            // catagory1TextBox
+            // 
+            this.catagory1TextBox.Location = new System.Drawing.Point(163, 3);
+            this.catagory1TextBox.Name = "catagory1TextBox";
+            this.catagory1TextBox.Size = new System.Drawing.Size(250, 20);
+            this.catagory1TextBox.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(838, 510);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.catagory1Button);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(425, 519);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(416, 26);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // catagory1Button
+            // 
+            this.catagory1Button.AutoSize = true;
+            this.catagory1Button.Location = new System.Drawing.Point(3, 3);
+            this.catagory1Button.Name = "catagory1Button";
+            this.catagory1Button.Size = new System.Drawing.Size(75, 23);
+            this.catagory1Button.TabIndex = 1;
+            this.catagory1Button.Text = "Catagory 1";
+            this.catagory1Button.UseVisualStyleBackColor = true;
+            this.catagory1Button.Click += new System.EventHandler(this.catagory1Button_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.directoryTextBox);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 615);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(416, 27);
+            this.flowLayoutPanel2.TabIndex = 6;
+            // 
+            // directoryTextBox
+            // 
+            this.directoryTextBox.Location = new System.Drawing.Point(163, 3);
+            this.directoryTextBox.Name = "directoryTextBox";
+            this.directoryTextBox.ReadOnly = true;
+            this.directoryTextBox.Size = new System.Drawing.Size(250, 20);
+            this.directoryTextBox.TabIndex = 3;
+            this.directoryTextBox.TextChanged += new System.EventHandler(this.directoryTextBox_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Filename,
+            this.ParentFolder});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(847, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(295, 510);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Filename
+            // 
+            this.Filename.Text = "Name";
+            this.Filename.Width = 175;
+            // 
+            // ParentFolder
+            // 
+            this.ParentFolder.Text = "Parent Folder";
+            this.ParentFolder.Width = 125;
+            // 
+            // PhotoOrganizer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 645);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "PhotoOrganizer";
+            this.Text = "Photo Catagorizer";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel8.ResumeLayout(false);
+            this.flowLayoutPanel8.PerformLayout();
+            this.flowLayoutPanel7.ResumeLayout(false);
+            this.flowLayoutPanel7.PerformLayout();
+            this.flowLayoutPanel6.ResumeLayout(false);
+            this.flowLayoutPanel6.PerformLayout();
+            this.flowLayoutPanel5.ResumeLayout(false);
+            this.flowLayoutPanel5.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel8;
+        private System.Windows.Forms.Button directoryButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel7;
+        private System.Windows.Forms.Button catagory2Button;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel6;
+        private System.Windows.Forms.Button catagory3Button;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel5;
+        private System.Windows.Forms.TextBox catagory3TextBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.TextBox catagory2TextBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.TextBox catagory1TextBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button catagory1Button;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox directoryTextBox;
+        private System.Windows.Forms.ColumnHeader Filename;
+        private System.Windows.Forms.ColumnHeader ParentFolder;
+    }
+}
+
---
+++ PhotoOrganizer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace photo_organizer {
+    public partial class PhotoOrganizer : Form {
+        public PhotoOrganizer() {
+            InitializeComponent();
+        }
+
+        private void directoryButton_Click(object sender,EventArgs e) {
+            FolderBrowserDialog folderPicker = new FolderBrowserDialog();
+            if(folderPicker.ShowDialog()==DialogResult.OK) {
+
+                listView1.Items.Clear();
+
+                string[] files = Directory.GetFiles(folderPicker.SelectedPath);
+                foreach(string file in files) {
+
+                    string fileName = Path.GetFileNameWithoutExtension(file);
+                    string parentFolder = Path.GetDirectoryName(file);
+                    parentFolder=parentFolder.Substring(parentFolder.LastIndexOf('\\')+1);
+                    string[] arr = new string[2];
+                    arr[0]=fileName;
+                    arr[1]=parentFolder;
+                    ListViewItem item = new ListViewItem(arr);
+                    item.Tag=file;
+
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+
+        private void directoryTextBox_TextChanged(object sender,EventArgs e) {
+
+        }
+
+        private void catagory1Button_Click(object sender,EventArgs e) {
+
+        }
+
+        private void catagory2Button_Click(object sender,EventArgs e) {
+
+        }
+
+        private void catagory3Button_Click(object sender,EventArgs e) {
+
+        }
+
+        public void DisplayFolder(string folderPath) {
+            string[] files = System.IO.Directory.GetFiles(folderPath);
+
+            for(int x = 0;x<files.Length;x++) {
+                listView1.Items.Add(files[x]);
+            }
+        }
+    }
+}
